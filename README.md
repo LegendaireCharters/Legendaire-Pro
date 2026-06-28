c877d3dc40d96d308b529c739e9e5edcad9f1df0
LegendaireCharters <rowleywalsh1@gmail.com>
2026-06-28 21:23:53 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Legendaire Charter Pro v7
+
+Upload all files in this folder to the root of your GitHub Pages repository.
+
+After upload, open:
+https://legendairecharters.github.io/legendaire-charter-pro/?v=7
+
+Then remove and re-add the Home Screen app if the old icon remains cached.
---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='lcp-v7-cache-20260628';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
